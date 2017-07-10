ddbb917da5807d711867a8f63a11ef2e6eb3e226
CortesV <vitya.semenyuk1995@gmail.com>
2017-07-10 11:56:29 +0300

    2

--- src/main/java/com/softbistro/orderbooks/TestController.java
+++ src/main/java/com/softbistro/orderbooks/TestController.java
@@ -12,4 +12,19 @@ public class TestController {
 	public String test() {
 		return "Hello";
 	}
+	
+	@RequestMapping(value = "Sasha", method = RequestMethod.GET, produces = "application/json")
+	public String test1() {
+		return "Sasha";
+	}
+	
+	@RequestMapping(value = "cortes", method = RequestMethod.GET, produces = "application/json")
+	public String test2() {
+		return "Cortes";
+	}
+	
+	@RequestMapping(value = "vlad", method = RequestMethod.GET, produces = "application/json")
+	public String test3() {
+		return "Vlad";
+	}
 }
