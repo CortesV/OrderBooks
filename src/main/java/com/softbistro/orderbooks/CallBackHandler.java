3c99ad5f3aea7104ed3d1837c772cf56b1c94bba
CortesV <vitya.semenyuk1995@gmail.com>
2017-07-10 22:39:53 +0300

    1

--- src/main/java/com/softbistro/orderbooks/CallBackHandler.java
+++ src/main/java/com/softbistro/orderbooks/CallBackHandler.java
@@ -262,6 +262,16 @@ public class CallBackHandler {
 				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg")
 				.buttons(secondLink).toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
 				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
+				.toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
+				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
+				.toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
+				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
+				.toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
+				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
+				.toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
+				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
+				.toList().addElement("Biology 12th edition").subtitle("Rent $19.49")				
+				.imageUrl("http://cs.cheggcdn.com/covers2/50310000/50318001_1484290068_Width288.jpg").buttons(thirdLink)
 				.toList().done().build();
 		this.sendClient.sendTemplate(recipientId, genericTemplate);
 	}
