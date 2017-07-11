c78ce2c83e9277f43d7b7e068012de925b1b16e2
CortesV <vitya.semenyuk1995@gmail.com>
2017-07-11 23:58:38 +0300

    1

--- src/test/java/com/softbistro/orderbooks/OrderBooksApplicationTests.java
+++ src/test/java/com/softbistro/orderbooks/OrderBooksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.softbistro.orderbooks;
+/*package com.softbistro.orderbooks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class OrderBooksApplicationTests {
 	}
 
 }
+*/
