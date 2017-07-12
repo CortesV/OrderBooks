2a30d77fc23e4491cfd4a2601103bc141912af3b
CortesV <vitya.semenyuk1995@gmail.com>
2017-07-12 16:33:09 +0300

    Messenger day 2

--- src/main/java/com/softbistro/orderbooks/service/TemplateService.java
+++ src/main/java/com/softbistro/orderbooks/service/TemplateService.java
@@ -208,13 +208,13 @@ public class TemplateService {
 		webResource = client.resource("http://80.91.191.79:19200/firstEvaluateCheckout");
 		response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON)
 				.post(ClientResponse.class, OrderCart.orderId);
-		OrderCart.shippingChoiceHash = new JSONObject(response.getEntity(String.class)).getString("shipping_choice");
+		OrderCart.shippingChoiceHash = new JSONObject(response.getEntity(String.class)).getString("shipping_choice").toString();
 		
 		
-		bookForOrder = new BookForOrder(Integer.valueOf(OrderCart.orderId), OrderCart.shippingChoiceHash);
+		/*bookForOrder = new BookForOrder(Integer.valueOf(OrderCart.orderId), OrderCart.shippingChoiceHash);
 		webResource = client.resource("http://80.91.191.79:19200/setShippingOpt");
 		response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON)
-				.post(ClientResponse.class, bookForOrder);
+				.post(ClientResponse.class, bookForOrder);*/
 	}
 	
 	public List<Book> readAll(String keyword) throws JsonParseException, JsonMappingException, IOException {
