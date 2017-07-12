f46fca6b70cadaafb6ac9cddc030b937574e3ed5
CortesV <vitya.semenyuk1995@gmail.com>
2017-07-12 16:42:51 +0300

    Messenger day 2

--- src/main/java/com/softbistro/orderbooks/service/TemplateService.java
+++ src/main/java/com/softbistro/orderbooks/service/TemplateService.java
@@ -211,7 +211,7 @@ public class TemplateService {
 		OrderCart.shippingChoiceHash = new JSONObject(response.getEntity(String.class)).getString("shipping_choice").toString();
 		
 		
-		bookForOrder = new BookForOrder(OrderCart.orderId, OrderCart.shippingChoiceHash);
+		bookForOrder.setShippingChoiceHash(OrderCart.shippingChoiceHash);
 		webResource = client.resource("http://80.91.191.79:19200/setShippingOpt");
 		response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON)
 				.post(ClientResponse.class, bookForOrder);
